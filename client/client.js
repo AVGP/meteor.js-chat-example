798d6c2dac737f4a430671a312511c33b440382c
Scott <d.scott.carroll@gmail.com>
2012-05-02 00:24:05 -0300

    delete rooms, author class, simple cleanup

--- client/client.js
+++ client/client.js
@@ -2,34 +2,37 @@ var Rooms = new Meteor.Collection("rooms");
 var Messages = new Meteor.Collection("messages");
 
 Template.rooms.events = {
-    "click #addRoom": function () {
-        var roomName = window.prompt("Name for the new name", "My room");
-        if(roomName != "") {
+    "click #addRoom": function (){
+        var roomName = window.prompt("Name for the new room.", "My room");
+        if(roomName) {
             Rooms.insert({"name": roomName});
         }
     }
 };
 
-Template.main.currentRoom = function() {
+Template.main.currentRoom = function (){
     return Session.get("room") || false;
-}
+};
 
 Template.rooms.availableRooms = function () {
     return Rooms.find({});
 };
 
 Template.roomItem.events = {
-    "click a.enter": function () {
+    "click a.enter": function (){
+        var name;
         if(Session.get("name") === undefined) {
-            var name = window.prompt("Your name", "Anonymous Coward");
-            if(name === "") name = "Jerky";
+            name = window.prompt("Your name", "Anonymous Coward") || "Jerky";
             Session.set("name", name);
         }
         Session.set("room", this._id);
-    }
+    },
+    "click .delete": function (){
+		Rooms.remove({_id: this._id});
+	}
 };
 
-Template.room.roomName = function() {
+Template.room.roomName = function (){
      var room = Rooms.findOne({_id: Session.get("room")});
      return room && room.name ;
 };
@@ -37,18 +40,28 @@ Template.room.roomName = function() {
 Template.room.messages = function() {
     return Messages.find({room: Session.get("room")});
 };
+Template.messageItem.authorClass = function (){
+    return Session.equals("name", this.author) ? ' mine' : '';
+};
 
 Template.room.events = {
     "click #leave": function() {
-        if(!window.confirm("Leave this room", "Do you really want to leave?")) return;
+        if(!window.confirm("Leave this room", "Do you really want to leave?")) { return; }
         Session.set("room", undefined);
     },
     "submit": function() {
-        Messages.insert({
-            "room": Session.get("room"),
-            "author": Session.get("name"),
-            "text": $("#msg").val()
-        });
-        $("#msg").val("");
+        var $msg = $("#msg");
+        if ($msg.val()){
+            Messages.insert({
+                "room": Session.get("room"),
+                "author": Session.get("name"),
+                "text": $("#msg").val(),
+    			"timestamp": (new Date()).toUTCString()
+            });
+        }
+        $msg.val("");
+        $msg.focus();
+    	Meteor.flush()
+		$("#messages").scrollTop(99999);
     }
 };
